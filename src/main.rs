d2412022fb65827157074348a1aa80752fbadbbb
luanngominh <luannm2@vng.com.vn>
2020-08-04 00:20:22 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// rustmap -b banner -a ip_address -c chunk_size -u limit_file_description -v
+// -b boolean: show banner
+// -a string: ip_address or domain name
+// -c integer: chunk size, describe how many port will scan at the time
+// -u interger: max open file
+// -v boolean: verbose
+
+fn main() {
+    println!("Hello, world!");
+}
